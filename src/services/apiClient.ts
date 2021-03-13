5019c958c3b34719ca0f4358477e8973be9a2780
Rodrigo Russo <68329584+RodrigoGaluppo@users.noreply.github.com>
2021-03-13 23:13:19 +0000

    fix
    fix

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"https://my-yahoo-newsscraper.herokuapp.com/" ,
+    baseURL:"https://my-yahoo-newsscraper.herokuapp.com/",
 })
 
 export default api
